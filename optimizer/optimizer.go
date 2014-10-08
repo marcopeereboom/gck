102a85f122ee6f0e862e80438ab392d50dbd3e33
Marco Peereboom <marco@conformal.com>
2014-10-08 14:27:23 -0500

    doco

--- optimizer/optimizer.go
+++ optimizer/optimizer.go
@@ -4,6 +4,8 @@ package optimizer
 import "github.com/marcopeereboom/gck/ast"
 
 // Optimize transforms n AST and returns an optimized version of it.
+// Currently optimizer returns a clone of the AST.
+// This is at the moment just a placeholder.
 func Optimize(n ast.Node) (ast.Node, error) {
 	return ast.Clone(n), nil
 }
